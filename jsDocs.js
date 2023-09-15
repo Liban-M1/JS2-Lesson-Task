d8e8509ab046c1c21ddad46ebb1c63311daaeb5a
Liban-M1 <114482781+Liban-M1@users.noreply.github.com>
2023-09-15 06:17:49 +0200

    .

---
+++ jsDocs.js
@@ -0,0 +1,23 @@
+
+
+/** 
+ * @typedef {Number | String} NumberLike
+ */
+
+/**
+ * This function adds two numbers together
+ * @param {NumberLike} a 
+ * @param {NumberLike} b 
+ * @returns a + b
+ * example: addNumbers(1, 2) // 3
+ * ```js
+ * // COMMENT HERE
+ * const result = addNumbers(1, 2) // 3
+ * ```
+ */
+function addNumbers(a, b) {
+    const result = a + b;
+    return result;
+  }
+
+  add
---
+++ main.js
@@ -0,0 +1,7 @@
+
+
+const values = new Set();
+values.add (1, 2, 2, 2, 3, 4,);
+console.log(values.size);
+console.log(values.has(1));
+console.log(values.has("a"));
