6fd09bd879bf2f818efacbda38f04d4861b3c697
Liban-M1 <114482781+Liban-M1@users.noreply.github.com>
2023-10-03 00:04:48 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 
 
-const values = new Set();
-values.add (1, 2, 2, 2, 3, 4,);
-console.log(values.size);
-console.log(values.has(1));
-console.log(values.has("a"));
+
+function makeFunction(x) {
+    return function (y) {
+      return x + y;
+    };
+  }
+  
+  const add5 = makeFunction(5);
+  
+  console.log(add5(2));
+  console.log(add5(5));
+  
+  const add10 = makeFunction(10);
+  console.log(add10(20));
